152ad97c3694b87dea677ae9b0182d495593958a
w-dan <d.galgora@alumnos.upm.es>
2022-05-30 15:30:55 +0200

    Added notes

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,8 @@
+/********************************************** */
+/*                      ready                    /
+/* logs ready when connected and operational     /
+/************************************************/
+
 module.exports = async(client) =>{
     console.log(`Bot $(client) is online`);
 };
